c351bf7c0deeecd6e9dbc4ec4bbb7f1b6fe5837e
machiechie <heyderli_005@mail.ru>
2026-04-06 18:06:24 +0000

    commit message

--- web_application_security/0x0b_web_application_fast_incident_response/2-count_attack.sh
+++ web_application_security/0x0b_web_application_fast_incident_response/2-count_attack.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # Identify the top attacker IP and count their total requests
 
-awk '{print $1}' $1 | sort | uniq -c | sort -nr | head -1 | awk '{print $1}'
+awk '{print $1}' logs.txt | sort | uniq -c | sort -nr | head -1 | awk '{print $1}'
