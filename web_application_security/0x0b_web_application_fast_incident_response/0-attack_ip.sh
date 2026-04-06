35355d3743e9e86051c542bb699d7430e1b5aa3f
machiechie <heyderli_005@mail.ru>
2026-04-06 17:57:50 +0000

    commit message

--- web_application_security/0x0b_web_application_fast_incident_response/0-attack_ip.sh
+++ web_application_security/0x0b_web_application_fast_incident_response/0-attack_ip.sh
@@ -2,4 +2,4 @@
 # Identify the IP address with the highest number of requests in a log file
 # Usage: ./0-attack_ip.sh logs.txt
 
-awk '{print $1}' $1 | sort | uniq -c | sort -nr | head -1 | awk '{print $2}'
+awk '{print $1}' logs.txt | sort | uniq -c | sort -nr | head -n 1 | awk '{print $2}'
