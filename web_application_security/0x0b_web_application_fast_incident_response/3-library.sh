86c28217d2000f681206eacce7907d5fa1f80fd4
machiechie <heyderli_005@mail.ru>
2026-04-06 18:16:00 +0000

    commit message

--- web_application_security/0x0b_web_application_fast_incident_response/3-library.sh
+++ web_application_security/0x0b_web_application_fast_incident_response/3-library.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # Identify the User-Agent library used by the top attacker
 
-awk '{print $12}' logs.txt | sort | uniq -c | sort -nr | head -1 | awk '{print $2}'
+awk '{print $6}' logs.txt | sort | uniq -c | sort -nr | head -1 | awk '{print $2}'
